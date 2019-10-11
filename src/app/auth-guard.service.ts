36cd2dcad5628104424edf177ab0a7ca178e33d8
Amir Hamza <hamza_amir@yahoo.com>
2019-10-11 09:27:49 +0600

    after login which page will be shown

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,20 +1,21 @@
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 	constructor(private auth: AuthService, private router: Router) {}
 
-	canActivate() {
+	canActivate(route, state: RouterStateSnapshot) {
 		return this.auth.user$.pipe(
 			map((user) => {
 				if (user) {
 					return true;
 				}
-				this.router.navigate([ '/login' ]);
+				this.router.navigate([ '/login' ], {
+					queryParams: { returnUrl: state.url }
+				});
 				return false;
 			})
 		);
